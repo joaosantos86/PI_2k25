a04cc6c2dad330f2f3fc36678adc9d2244e828d6
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2025-09-16 23:00:28 -0300

    Esquece, você precisa esquecer essa vingança. Confie em mim. Aqueles que seguem o caminho da vingança nunca acabam bem, você só se destrói, e ainda que tenha sucesso você se vinga. E o que sobrará com isso? Nada! Um vazio. kakashi frases

--- js/sobrenos.js
+++ js/sobrenos.js
@@ -1,11 +1,12 @@
 const contribuicao = {
-    Otavio: "desenvolveu as páginas: sobre nós, receitas e termos de uso.",
-    Jota: "",
-    Miguel: "",
-    Sofia: "",
-    Alice: "",
-    Flavia: ""
+    Otavio: "Desenvolveu as páginas: Sobre nós, Receitas, Imc e Termos de uso.",
+    jota: "Desenvolveu a página: Sobre o projeto e fez o design de todas as páginas.",
+    Miguel: "Desenvolveu as páginas: Home e Curiosidades.",
+    sofia: "Cuidou das redes sociais",
+    Alice: "Cuidou das redes sociais",
+    Flavia: "Cuidou das redes sociais"
 };
+//fiz uns texto meio provisorio não sei direito o que cada um fez, dai muda depois .
 
 // to construindo o javascript ainda, NÂO QUERO CRITICAS !!!!
 function contri(id) {
@@ -14,4 +15,4 @@ function contri(id) {
     if (msg) {
         msg.textContent = contribuicao[id];
     }
-}
+}  
